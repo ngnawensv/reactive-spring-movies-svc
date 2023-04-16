fd0853be4b0c5ca4abfa3b118f4c2cf00e66266b
Ngnawen Samuel <ngnawens@gmail.com>
2023-04-16 17:21:28 -0400

    business logic done

---
+++ src/main/java/moviessvc/ReactiveSpringMoviesSvcApplication.java
@@ -0,0 +1,13 @@
+package moviessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringMoviesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringMoviesSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moviessvc/ReactiveSpringMoviesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package moviessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringMoviesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
